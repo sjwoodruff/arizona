ee69622ed0cc6ebccadd2da8bfec3eb65bda3dc8
Sterling Woodruff <sterlingwoodruff@mail.weber.edu>
2017-03-16 16:22:32 +0000

    Initial commit task1

---
+++ arizona_hw7_task1.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  arizona_hw7_task1.c
+ *       	Usage:  ./arizona_hw7_task1.c
+ *    Description:  Task1
+ *
+ *        Version:  1.0
+ *        Created:  03/16/2017 04:21:37 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Sterling Woodruff (), sterlingwoodruff@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+/* Function Prototypes */
+
+/* Main Program */
+int main (int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
