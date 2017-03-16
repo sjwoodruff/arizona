d153e95a7ce0670fd8feb2122b475a6d4f532d0e
ahlweb <adamlee1@mail.weber.edu>
2017-03-16 16:25:02 +0000

    inital commit

---
+++ arizona_hw7_task2.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  arizona_hw7_task2.c
+ *       	Usage:  ./arizona_hw7_task2.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/16/2017 04:23:57 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Adam Lee (), Adamlee1@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
