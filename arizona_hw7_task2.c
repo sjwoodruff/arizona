68d51defa0c8759438d68c9695289598bfba429e
ahlweb <adamlee1@mail.weber.edu>
2017-03-22 05:41:26 +0000

    max min func done

--- arizona_hw7_task2.c
+++ arizona_hw7_task2.c
@@ -3,7 +3,7 @@
  *
  *       Filename:  arizona_hw7_task2.c
  *       	Usage:  ./arizona_hw7_task2.c
- *    Description:  
+ *    Description: Random array
  *
  *        Version:  1.0
  *        Created:  03/16/2017 04:23:57 PM
@@ -13,9 +13,33 @@
  */
 #include <stdio.h>		/* For Standard I/O */
 #include <stdlib.h>
+#include <time.h>
+
+#define MAXNUM 10
+/*function Prototype*/
+
+void MaxMin(int numvals, int vals[], int* min, int* max);
+	int min,max;
+
 /* Main Program */
 int main(int argc, char *argv[])
 {
+	srand(time(NULL));
+	int randval[MAXNUM] = {rand(),1,2,3,4,5,6,7,8,9}; 
+
+	printf("Generating random list \n\n");
+	printf("This list is: \n\n");
+	for (int i = 0; i < MAXNUM; i++)
+	{
+
+	printf("%d ", randval[i]);
+	}
+	MaxMin(MAXNUM,randval,&min,&max);
+	
+	printf("\n\nThe maximum value is: %d\n\n", max);
+	printf("The minimum value is: %d\n\n", min);
+
+
 
 	return 0;
 }
@@ -23,4 +47,31 @@ int main(int argc, char *argv[])
 
 /* Function Defenitions */
 
+void MaxMin(int numvals, int vals[], int* min, int* max)
+{
+	*max = vals[0];
+	*min = vals[0];
+
+	for (int j = 1; j < numvals; j++)
+	{
+		if (*min > vals[j])
+		{
+			*min = vals[j];
+		}
+	}
+
+	for (int i = 1; i < numvals; i++)
+	{
+		if (*max < vals[i])
+		{
+			*max = vals[i];
+		}
+	}
+
+
+//	int numvals[MAXNUM] = {rand(2)}; 
+
+
+	return;
+}
 
